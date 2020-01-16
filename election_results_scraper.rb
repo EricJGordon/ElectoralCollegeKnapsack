90a49320aa94af87fa29efeb3f1e831ab14f5ed8
Eric Gordon <eric.gordon@ucdconnect.ie>
2020-01-16 13:08:29 +0000

    Now prints out state name, vote totals, and electoral votes for each state, correctly handling rows with an atypical number of elements.

--- election_results_scraper.rb
+++ election_results_scraper.rb
@@ -10,17 +10,24 @@ class ElectionResultsScraper
 
   output = @parsed_page.css(".wikitable")[14].css("tbody").css("tr").css("td").children.map {|child| child.text}
 
+  print output
   # print @parsed_page
+  j = 0
   55.times do |i|           # 55 from 50 states, plus 2 Maine congressional districts, and 3 Nebraska ones
-    puts output[26*i]
-    puts output[26*i + 2]
-    puts output[26*i + 4]
-    puts output[26*i + 5]
-    puts output[26*i + 7]
-    puts "_______________"
-    puts
+    puts output[j]
+    until output[j + 2] =~ /[0-9]/  # For handling variable number of 'td's between state name and vote numbers
+      j += 1                        # due to additional details provided for states that split their electoral votes
+    end                             # by congressional district, specifically Maine and Nebraska
+    puts output[j + 2]
+    puts output[j + 4]
+    puts output[j + 5]
+    puts output[j + 7]
+    j += 24
+    while output[j][0] == "[" || output[j][0] == "\n" # For handling a variable number of citations per row
+      j += 1                                       # skips each one, then skips newline, leaving j at start of next row
+    end
+    puts "_______________\n"
   end
-  # TODO: handle different number of citations per row, currently throwing off pattern of every 26th element
 
   # print "test"
 end
