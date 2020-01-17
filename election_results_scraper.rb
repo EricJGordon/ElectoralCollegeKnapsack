e34356fe24c6d20d2370e3c955c3a2d4d155c0ea
Eric Gordon <eric.gordon@ucdconnect.ie>
2020-01-17 00:43:14 +0000

    Increased number of iterations by one to account for inclusion of DC. Added subsequently necessary check to prevent NilClass issues

--- election_results_scraper.rb
+++ election_results_scraper.rb
@@ -8,12 +8,12 @@ class ElectionResultsScraper
   @url2 = "https://en.wikipedia.org/w/index.php?title=Template:State_results_of_the_2016_U.S._presidential_election"
   @parsed_page = Nokogiri::HTML(open(@url))
 
-  output = @parsed_page.css(".wikitable")[14].css("tbody").css("tr").css("td").children.map {|child| child.text}
+  output = @parsed_page.css(".wikitable")[14].css("tbody").css("tr").css("td").children.map(&:text)
 
   print output
   # print @parsed_page
   j = 0
-  55.times do |i|           # 55 from 50 states, plus 2 Maine congressional districts, and 3 Nebraska ones
+  56.times do |i|     # 56 from 50 states, plus Washington DC, plus 2 Maine congressional districts, and 3 Nebraska ones
     puts output[j]
     until output[j + 2] =~ /[0-9]/  # For handling variable number of 'td's between state name and vote numbers
       j += 1                        # due to additional details provided for states that split their electoral votes
@@ -23,7 +23,7 @@ class ElectionResultsScraper
     puts output[j + 5]
     puts output[j + 7]
     j += 24
-    while output[j][0] == "[" || output[j][0] == "\n" # For handling a variable number of citations per row
+    while i!=55 && (output[j][0] == "[" || output[j][0] == "\n") # For handling a variable number of citations per row
       j += 1                                       # skips each one, then skips newline, leaving j at start of next row
     end
     puts "_______________\n"
