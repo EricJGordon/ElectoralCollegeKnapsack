1206a3e99c52b116b4db8ed3836e2d3c2ccc1094
Eric Gordon <eric.gordon@ucdconnect.ie>
2020-01-19 22:45:21 +0000

    Created csv file with 2016's results

--- election_results_scraper.rb
+++ election_results_scraper.rb
@@ -8,35 +8,40 @@ class ElectionResultsScraper
   @url2 = "https://en.wikipedia.org/wiki/2012_United_States_presidential_election"
   @url3 = "https://en.wikipedia.org/wiki/2000_United_States_presidential_election"
   # @other = "https://en.wikipedia.org/w/index.php?title=Template:State_results_of_the_2016_U.S._presidential_election"
-  @parsed_page = Nokogiri::HTML(open(@url3))
+  @parsed_page = Nokogiri::HTML(open(@url))
 
-  #  output = @parsed_page.css(".wikitable")[14].css("tbody").css("tr").css("td").children.map(&:text)     # 2016
+   output = @parsed_page.css(".wikitable")[14].css("tbody").css("tr").css("td").children.map(&:text)     # 2016
   #  output = @parsed_page.css(".wikitable")[6].css("tbody").css("tr").css("td").children.map(&:text)        # 2012
   # output = @parsed_page.css(".wikitable")[7].css("tbody").css("tr").css("td").children.map(&:text)        # 2008
   # output = @parsed_page.css(".wikitable")[5].css("tbody").css("tr").css("td").children.map(&:text)        # 2004
-  output = @parsed_page.css(".wikitable")[5].css("tbody").css("tr").css("td").children.map(&:text)        # 2000
+  # output = @parsed_page.css(".wikitable")[5].css("tbody").css("tr").css("td").children.map(&:text)        # 2000
 
   print(output, "\n\n")
   # print @parsed_page
 
-  j = 0
-  56.times do |i|     # 56 from 50 states, plus Washington DC, plus 2 Maine congressional districts, and 3 Nebraska ones
-    until output[j][0] =~ /[A-Z]/ && output[j][1] =~ /[a-z]/  # For handling a variable number of citations/blank spaces
-      j += 1                                                  # per row. Skips each one, leaving j at next state's name
+  CSV.open("2016results.csv", "wb") do |csv|
+    csv << ["D_votes", "R_votes", "EVs"]
+    j = 0
+    56.times do |i|     # 56 from 50 states, plus Washington DC, plus 2 Maine congressional districts, and 3 Nebraska ones
+      row = []
+      until output[j][0] =~ /[A-Z]/ && output[j][1] =~ /[a-z]/  # For handling a variable number of citations/blank spaces
+        j += 1                                                  # per row. Skips each one, leaving j at next state's name
+        break if output[j].nil?
+      end
+      row << output[j].delete(',')
+      until output[j + 1] =~ /,/         # For handling a variable number of elements between state name and vote numbers
+        j += 1                           # due to additional details provided for states that split their electoral votes
+      end                                # by congressional district, specifically Maine and Nebraska
+      row << output[j + 1].delete(',')  # order of dem/rep votes should be consistent, regardless of wikitable's order,
+      row << output[j + 4].delete(',')  # automatically determine by checking header?
+      row << (output[j + 3]!='–' ? output[j + 3] : output[j + 6])
+      csv << row
+      j += 22
+      # puts "_______________\n"
       break if output[j].nil?
     end
-    puts output[j]
-    until output[j + 1] =~ /,/         # For handling a variable number of elements between state name and vote numbers
-      j += 1                           # due to additional details provided for states that split their electoral votes
-    end                                # by congressional district, specifically Maine and Nebraska
-    puts output[j + 1]
-    puts output[j + 3]
-    puts output[j + 4]
-    puts output[j + 6]
-      j += 22
-    puts "_______________\n"
-    break if output[j].nil?
   end
+
 
   # print "test"
 end
