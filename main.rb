bb4776938c692bca4a1b5d3633e4f8c159b73727
Eric Gordon <eric.gordon@ucdconnect.ie>
2020-02-14 23:22:46 +0000

    *Finally* got knapsack function fully working such that it also keeps track of which states contribute to the result

--- main.rb
+++ main.rb
@@ -19,8 +19,11 @@ puts("\nDem electoral votes: #{dem_EVs}")
 puts("Rep electoral votes: #{rep_EVs}")
 puts("Winner is the #{(dem_EVs>rep_EVs)?"Democrat":"Republican"}")
 
-def knapsack(items, c)
+def knapsack(items, weight)
+
+
   chosen = Array.new
+=begin
   if @solved[items.size][c].nil? && items.size > 0 && c!=0
     #puts items.size
     if items[-1][:weight] > c
@@ -32,11 +35,38 @@ def knapsack(items, c)
       tmp2 << items[-1]
       #puts("tmp1:   #{tmp1}")
       #puts("tmp2:   #{tmp2}")
-      val1 = tmp1.reduce(0) {|sum, x| sum + x[:value]}
-      val2 = tmp2.reduce(0) {|sum, x| sum + x[:value]}
+
       chosen = val1>val2 ? tmp1 : tmp2
     end
     @solved[items.size][c] = chosen
+  end
+  chosen
+=end
+
+
+  0.upto(items.size) do |i|
+    0.upto(weight) do |w|
+      if i == 0 || w == 0
+        @solved[i][w] = 0
+      elsif items[i - 1][:weight] <= w
+        # tmp1 = @solved[i - 1][w]
+        # tmp2 = @solved[i - 1][w - items[i - 1][:weight]] << items[i - 1]
+        # val1 = tmp1.reduce(0) {|sum, x| sum + x[:value]}
+        # puts("i = #{i}, w = #{w}")
+        # puts("tmp1:   #{tmp1}\n(size = #{tmp1.size})")
+        # puts("tmp2:   #{tmp2}\n(size = #{tmp2.size})")
+        #val2 = tmp2.reduce(0) {|sum, x| sum + x[:value]}
+        @solved[i][w] = [@solved[i - 1][w], @solved[i - 1][w - items[i - 1][:weight]] + items[i - 1][:value]].max
+      else
+        @solved[i][w] = @solved[i - 1][w];
+      end
+    end
+  end
+  items.length.downto(0) do |i|
+    if @solved[i][weight] != @solved[i - 1][weight]
+        weight -= items[i - 1][:weight]
+    chosen << items[i - 1]
+    end
   end
   chosen
 end
@@ -51,48 +81,11 @@ items = Array.new
 items << item1 << item2 << item3 << item4 << item5 << item6
 @solved = Array.new(states.size + 1) {Array.new(269)}
 
-puts states
+#puts states
 ans = knapsack(states.shuffle, 268)
 puts("Answer: ", ans)
 puts("EVs:  #{ans.reduce(0) {|sum, x| sum + x[:weight]}}")
 puts("votes needed to scrape a plurality in each of those states:  #{ans.reduce(0) {|sum, x| sum + x[:value]}}")
 puts("Comparable number of votes in the states that made up the remaining 270 EVs: #{states.reduce(0) {|sum, x| sum + x[:value]} - ans.reduce(0) {|sum, x| sum + x[:value]}}")
-puts("Most efficient states to win EC: ",  states.to_a - ans.to_a)
+puts("Most efficient states to win EC: ",  (states.to_a - ans.to_a).map { |s| "#{s[:value]} votes in #{s[:name]}" })
 
-@arr = Array.new(states.size + 1) {Array.new(269)}
-@w = Array.new
-@v = Array.new
-def KS(n, c)
-  if !@arr[n][c].nil?
-    return @arr[n][c]
-  end
-  if n == 0 || c == 0
-    result = 0
-  elsif @w[n] > c
-    result = KS(n-1, c)
-  else
-    tmp1 = KS(n-1, c)
-    tmp2 = @v[n] + KS(n-1, c-@w[n])
-    #puts("tmp2:   #{tmp2}")
-    result = [tmp1, tmp2].max
-  end
-  @arr[n][c] = result
-  result
-end
-
-@w[0], @v[0] = 0, 0
-
-states = states.shuffle
-states.each do |item|
-  @w << item[:weight]
-  @v << item[:value]
-end
-
-puts(knapsack(items, 10))
-puts "------"
-puts(KS(states.size, 268))
-
-@arr.each do |row|
-  print row*', '
-  puts
-end
